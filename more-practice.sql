38d7ab19ef5249a19de9ab4101531ddba3dc4db6
Laurel <lkorwin@gmail.com>
2016-07-30 21:58:10 +0000

    Finished more practice file

---
+++ more-practice.sql
@@ -0,0 +1,19 @@
+-- Include your solutions to the More Practice problems in this file.
+
+
+
+-- INSERT
+INSERT INTO models (year, brand_name, name) VALUES ('2015', 'Chevrolet', 'Malibu');
+INSERT INTO models (year, brand_name, name) VALUES ('2015', 'Subaru', 'Outback');
+
+-- CREATE TABLE
+CREATE TABLE awards (
+    name VARCHAR(30),
+    year INTEGER,
+    winner_id INTEGER PRIMARY KEY
+);
+
+-- More INSERT
+
+INSERT INTO awards VALUES ('IIHS Safety Award', 2015, 49);
+INSERT INTO awards VALUES ('IIHS Safety Award', 2015, 50);
